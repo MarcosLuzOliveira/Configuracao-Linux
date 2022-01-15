521b7836eadbbaa35678e154fd08944611f195d0
Marcos da Luz Oliveira <marcosluz312@gmail.com>
2022-01-15 13:58:44 -0300

    v 1

--- linuxC.sh
+++ linuxC.sh
@@ -10,3 +10,4 @@ echo -e "\n. $HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo -e "\n. $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
 sudo apt-get install gnome-tweak-tool
 
+sudo sed -i 's/modifier_map Mod3/\/\/ modifier_map Mod3/g' /usr/share/X11/xkb/symbols/br
